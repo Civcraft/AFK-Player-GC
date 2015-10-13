f1b032921eccff588a71f307eeb79a5b869e714a
Daniel Boston <programmerdan@gmail.com>
2015-10-12 22:55:43 -0400

    Turns out, you have to schedule the second teleport, or bukkit ignores it when leaving a vehicle. Fun.

--- src/com/github/Kraken3/AFKPGC/commands/LagInvestigation.java
+++ src/com/github/Kraken3/AFKPGC/commands/LagInvestigation.java
@@ -3,12 +3,16 @@ package com.github.Kraken3.AFKPGC.commands;
 import org.bukkit.command.CommandSender;
 
 import java.util.List;
+import java.util.UUID;
 
 import com.github.Kraken3.AFKPGC.AFKPGC;
 import com.github.Kraken3.AFKPGC.LagScanner;
 
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Entity;
+import org.bukkit.Bukkit;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import org.apache.commons.lang.mutable.MutableLong;
 
@@ -23,13 +27,23 @@ public class LagInvestigation extends AbstractCommand {
 		if (sender instanceof Player) {
 			Player p = (Player) sender;
 			MutableLong chunkWeight = new MutableLong(0L);
-			Location nextUp = LagScanner.getNextBadChunk(chunkWeight, true);
+			final Location nextUp = LagScanner.getNextBadChunk(chunkWeight, true);
 			if (nextUp != null) {
 				sender.sendMessage("Teleporting you to an uninvestigated Lag area (weight " + chunkWeight.longValue() + "): " + nextUp.toString());
 				if (p.isInsideVehicle()) {
-					p.leaveVehicle();
+					if (!p.leaveVehicle()) {
+						Entity veh = p.getVehicle();
+						veh.eject();
+					}
 				}
-				p.teleport(nextUp);
+				final UUID playerUUID = p.getUniqueId();
+				new BukkitRunnable() {
+						@Override
+						public void run() {
+							Player q = Bukkit.getPlayer(playerUUID);
+							q.teleport(nextUp);
+						}
+				}.runTaskLater(super.plugin, 2);
 			} else {
 				sender.sendMessage("Nothing left to investigate");
 			}
